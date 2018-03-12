8ac99e8d28eff42a0fb5205a0b85fc80a00f61e5
Alice Wonder <github@domblogger.net>
2018-03-12 03:53:04 -0700

    works now

--- bin/jQueryCDN.php
+++ bin/jQueryCDN.php
@@ -3,25 +3,19 @@
 
 $confdir = dirname( dirname(__FILE__) ) . '/etc';
 
-//echo $confdir;
-
 $files = scandir($confdir);
-
-//var_dump($files);
 
 foreach($files as $file) {
   $suffix = substr($file, -5);
   if($suffix === '.dist') {
     $dist = $confdir . '/' . $file;
     $new = $confdir . '/' . preg_replace('/\.dist$/', '', $file);
-    var_dump($new);
-    sleep(5);
     if($STRING = file_get_contents($dist)) {
       if($json = json_decode($STRING)) {
         if($json->name === 'jquery') {
           $srcurl = preg_replace('/\/js/', 'https://code.jquery.com', $json->srcurl);
           $json->srcurl = $srcurl;
-          $STRING = json_encode($json, JSON_PRETTY_PRINT);
+          $STRING = json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
           if($handle = @fopen($new, 'w')) {
             fwrite($handle, $STRING);
             fclose($handle);
@@ -33,37 +27,5 @@ foreach($files as $file) {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
